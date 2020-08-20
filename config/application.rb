e753867e41456bc14a8fc0ca59db2eac5dc6341c
kuritaeiji <eiji199825@yahoo.co.jp>
2020-08-20 19:02:03 +0900

    no message

--- config/application.rb
+++ config/application.rb
@@ -11,9 +11,10 @@ module FreemarketApp
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
 
-    # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration can go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded after loading
-    # the framework and any gems in your application.
+    config.generators do |g|  # ここから追記
+      g.assets false          # CSS, JavaScriptファイル生成せず
+      g.skip_routes false     # trueならroutes.rb変更せず、falseなら通常通り変更
+      g.test_framework false  # testファイル生成せず
+    end                       # ここま
   end
 end
